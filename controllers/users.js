f025c71c575700ee3fd8957be0e1b75b66b010d3
Sogan87 <sandro.leonardo.ti@gmail.com>
2023-05-18 14:19:25 -0300

    comando put

--- controllers/users.js
+++ controllers/users.js
@@ -21,3 +21,14 @@ export const deleteUser = (req, res) => {
     users = users.filter((user) => user.id !== req.params.id);
     res.send("Usuario deletado");
 }
+
+export const updateUser = (req, res) => {
+    const user =  users.find((user) => user.id === req.params.id);
+
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.contact = req.body.contact;
+
+    res.send("Usuario atualizado com sucesso");
+
+}
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import {getUsers, createUser, getUser, deleteUser} from "../controllers/users.js"
+import {getUsers, createUser, getUser, deleteUser, updateUser} from "../controllers/users.js"
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get("/users", getUsers);
 router.post("/user", createUser);
 router.get("/user/:id", getUser);
 router.delete("/user/:id", deleteUser);
+router.put("/user/:id", updateUser);
 
 export default router;
