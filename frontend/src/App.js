bbff8df65a2b80f01dfbd364e1ddb15cb34fa3c1
Sogan87 <sandro.leonardo.ti@gmail.com>
2023-05-19 11:33:18 -0300

    Testando frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,13 @@ import AddEdit from './pages/AddEdit';
 import Home from './pages/Home';
 import View from './pages/View';
 import About from './pages/About';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Header/>
         <ToastContainer />
         <Routes>
           <Route path="/" element={<Home />} />
