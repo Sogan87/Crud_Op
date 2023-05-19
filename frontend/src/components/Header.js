81a2b0b452e36a8920936bea75bb1e28dbbd6496
Sogan87 <sandro.leonardo.ti@gmail.com>
2023-05-19 15:55:20 -0300

    Trabalhando no front

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,24 +3,46 @@ import {Link, useLocation} from "react-router-dom";
 import "./Header.css"
 
 const Header = () => {
-    const[activeTab, setActiveTab] ="Home";
-    return(
-        <div className="header">
-            <p className="logo">User Manegement System</p>
-            <div className="header-right">
-                <Link to="/">
-                    <p className={`${activeTab === "Home" ? "active" : ""}`} 
-                    onClick={() => setActiveTab("Home")} >Home</p>
-                </Link>
-                <Link to="/add">
-                    <p className={`${activeTab  === "AddUser" ? "active" : ""}`}
-                    onClick={() => setActiveTab("AddUser")}>Add User</p>
-                </Link>
-                <Link to="/about">
-                    <p className={`${activeTab  === "About" ? "active" : ""}`}
-                    onClick={() => setActiveTab("About")}>About</p>
+    const[activeTab, setActiveTab] = useState("Home");
 
-                </Link>                
+    const location = useLocation();
+    useEffect(() => {
+        if (location.pathname === "/") {
+            setActiveTab("Home");
+            } else if (location.pathname === "/add") {
+                setActiveTab("AddUser");
+                } else if (location.pathname === "/about") {
+                    setActiveTab("About");
+                }
+    },[location]);
+    return (
+        <div className="header">
+           <p className="logo">User Manegement System</p>
+           <div className="header-right">
+                <Link to="/">
+                    <p 
+                        className={`${activeTab === "Home" ? "active" : ""}`} 
+                        onClick={() => setActiveTab("Home")}
+                     >Home
+                    </p>
+                </Link>
+
+                <Link to="/add">
+                    <p 
+                        className={`${activeTab  === "AddUser" ? "active" : ""}`}
+                        onClick={() => setActiveTab("AddUser")}
+                    >Add User
+                    </p>
+                </Link>
+
+                <Link to="/about">
+                    <p 
+                        className={`${activeTab  === "About" ? "active" : ""}`}
+                        onClick={() => setActiveTab("About")}
+                    >About
+                    </p>
+                </Link> 
+
             </div>            
         </div>
     );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom'
+import "./Home.css";
+import axios from 'axios';
+import { getUsers } from '../../../controllers/users';
+import { async } from '@dabeng/react-orgchart';
+
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getUsers();
+  }, [])
+
+  const getUser = async () => {
+    const response = await axios.get('http://localhost:5000/users');
+    if (response.status === 200){
+      setData(response.data);
+    }
+  };
+
+  console.log("data=>", data);
+
+
   return (
-    <div>
-       <h2>Home</h2> 
+    <div style={{marginTop:"150px"}}>
+      <table className="styled-table">
+        <thead>
+        <tr>
+          <th style={{testAlign: "center"}}>No.</th>
+          <th style={{testAlign: "center"}}>Name.</th>
+          <th style={{testAlign: "center"}}>Email.</th>
+          <th style={{testAlign: "center"}}>Contact.</th>
+          <th style={{testAlign: "center"}}>Action.</th>
+        </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => {
+            return (
+              <tr key={index}>
+                <th scope="row">{index+1}</th>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.contact}</td>
+                <td>
+                  <Link to={`/update/${item.id}`}>
+                    <button className="btn btn-edit">Edit</button>
+                  </Link>
+                  <button className="btn btn-delete">Delete</button>
+                  <Link to={`/view/${item.id}`}>
+                    <button className="btn btn-view">View</button>
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>        
+      </table>
     </div>
   )
 }
